06c0c1c7afbe830e31f8b973e11b58c3318674df
Mukesh Selvaraj <abirami@ishamistygreen.com>
2023-04-24 16:59:38 +0530

    apiroutes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client';
+import { useApollo } from '../apollo/client';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  const apolloClient = useApollo(pageProps.initialApolloState);
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
